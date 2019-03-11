2b401f13a20f99b06aa4a9c1cc3708fdf6b3bbfa
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-03-11 10:15:51 -0500

    first commit

---
+++ assets/javascript/train.js
@@ -0,0 +1,26 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8ItiALhmTJ8gE4rXIC7mGx2GT6cvcMMg",
+    authDomain: "train-22661.firebaseapp.com",
+    databaseURL: "https://train-22661.firebaseio.com",
+    projectId: "train-22661",
+    storageBucket: "train-22661.appspot.com",
+    messagingSenderId: "386866090498"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  var queryURL = //Train API url and components + api key;
+
+
+  //Do we even need ajax for this?
+  /*$.ajax({
+
+      url: //queryURL
+      method: "GET"
+
+  }).then(function) {
+
+  }*/
