312e625a7820060c8b094f1b401bd29e8d39d58b
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-03-11 16:02:13 -0500

    second commit

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,5 @@
-  // Initialize Firebase
+$(document).ready(function() {
+//Firebase config initialization
   var config = {
     apiKey: "AIzaSyB8ItiALhmTJ8gE4rXIC7mGx2GT6cvcMMg",
     authDomain: "train-22661.firebaseapp.com",
@@ -11,16 +12,65 @@
 
   var database = firebase.database();
 
+    //on submit-button function
+      $("#add-train-btn").on("click", function(event) {
+          event.preventDefault();
+        //re-naming varibales with input value, with trim.
+          var name = $("#name-input").val().trim();
+          var destination = $("#destination-input").val().trim();
+          var firstTrain = $("#first-train-input").val().trim();
+          var frequency = $("#frequency-input").val().trim();
+        //push to firebase
+          var newTrain = {
+              name: name,
+              destination: destination,
+              firstTrain: firstTrain,
+              frequency: frequency,
+          };
+          database.ref().push(newTrain);
+          $("form")[0].reset();
+      });
 
-  var queryURL = //Train API url and components + api key;
+      function tableFunction () {
+        database.ref().on("child_added", function(childSnapshot) {
+          console.log(childSnapshot.val());
+          
+          var trainNameAdd = childSnapshot.val().name;
+          var trainDestinationAdd = childSnapshot.val().destination;
+          var trainFirstTrain = childSnapshot.val().firstTrain;
+          var trainFrequencyAdd = childSnapshot.val().frequency;
 
+            var firstTimeTransfered = moment(trainFirstTrain, "hh:mm").subtract(1, "years");
 
-  //Do we even need ajax for this?
-  /*$.ajax({
+            var currentTime = moment();
+            console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
-      url: //queryURL
-      method: "GET"
+            var differenceTime = moment().diff(moment(firstTimeTransfered), "minutes");
 
-  }).then(function) {
+            var remainder = differenceTime % trainFrequencyAdd;
 
-  }*/
+            var timeUntilTrain = trainFrequencyAdd - remainder;
+
+            var nextTrain = moment().add(timeUntilTrain, "minutes");
+
+            var nextTime = moment(nextTrain).format("hh:mm");
+
+            // $("tbody").append("<tr><td>" + trainNameAdd + "</td><td>" + trainDestinationAdd + "</td><td>" + trainFrequencyAdd + "</td><td>" + nextTime + "</td><td>" + timeUntilTrain + "</td></tr>");
+
+            
+            //Create the new row
+              var newRow = $("<tr>").append(
+                $("<td>").text(trainNameAdd),
+                $("<td>").text(trainDestinationAdd),
+                $("<td>").text(trainFrequencyAdd),
+                $("<td>").text(nextTime),
+                $("<td>").text(timeUntilTrain),
+              );
+
+              // Append the new row to the table
+              $("#train-table > tbody").append(newRow); 
+        });
+      }
+
+      tableFunction();
+});
